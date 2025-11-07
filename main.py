a34fe9311f55119b1fbc45c94df2210a76b9127f
igonzales <igonzales@ubx.ph>
2025-11-07 14:37:31 +0800

    fix: handle OPTIONS requests

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
 from typing import List
-
-# Initialize API
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -32,6 +30,23 @@ refresh_token = os.getenv("REFRESH_TOKEN")
 
 # Configure Gemini API Connection
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Initialize API
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://gemini-spotify-fe.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows requests from your frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Retrieve client/app/server.py token for API calls
 def get_client_token():
